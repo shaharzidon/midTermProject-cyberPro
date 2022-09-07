ece528f7b16997235a31bf1fec6872394dbd6270
Shahar Zidon <shaharsmacbookpro@Shahars-MacBook-Pro.local>
2022-09-07 16:52:59 +0300

    first commit shahar

---
+++ src/component_faq/Faq.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './faq.css'
+
+function Faq() {
+    return (
+        <div className='faq'>
+            <div className='upperSection'>
+                <h1>שאלות נפוצות</h1>
+            </div>
+            <div className='bottomSection'>
+                <div className='bottomSection-content'>
+                    <div className='faq1'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq2'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq3'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq3'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq4'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq5'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+                    <div className='faq6'>
+                        <h3>?שאלה</h3>
+                        <p>תשובה</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Faq;
+
---
+++ src/component_ourImpact/OurImpact.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './ourImpact.css'
+function OurImpact() {
+    return (
+        <div className='our-impact'>
+            <h1>ההשפעה שלנו</h1>
+            <div className='upper-section'>
+                <div className='left-section'>
+                    <div className='left-section-img'>
+                        <h3>add a logo</h3>
+
+                    </div>
+                    <div className='left-section-text'>
+                        <div>יש לנו</div>
+                        <div>4534535</div>
+                        <div>מתנדבים</div>
+                    </div>
+
+                </div>
+                <div className='right-section'>
+                    <div className='right-section-img'>
+                        <h3>add a logo</h3>
+                    </div>
+                    <div className='right-section-text'>
+                        <div>:ביחד, ניקינו יותר מ</div>
+                        <div>120</div>
+                        <div>ק״ג של זבל ממקורות המים בישראל</div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className='bottom-section'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur possimu
+                    impedit esse magnam eveniet architecto enim amet quos reprehenderit iusto,
+                    animi quidem illo. Minima asperiores dicta voluptatem reiciendis
+                    laudantium non?</p>
+            </div>
+        </div>
+    );
+}
+export default OurImpact;
+
